be82f8797f14bc2cdddddc68ca47b92e6268830d
Shoki <syoki0415@gmail.com>
2025-08-29 16:20:33 +0900

    commit

--- src/main/java/jp/te4a/app2/facility2/controller/AdminController.java
+++ src/main/java/jp/te4a/app2/facility2/controller/AdminController.java
@@ -1,5 +1,10 @@
 package jp.te4a.app2.facility2.controller;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import jp.te4a.app2.facility2.bean.FacilityBean;
 import jp.te4a.app2.facility2.form.FacilityForm;
 import jp.te4a.app2.facility2.service.FacilityService;
 
@@ -30,6 +37,30 @@ public class AdminController {
     FacilityForm setUpForm() {
         return new FacilityForm();
     }
+    //管理者ログイン時の検索
+    @PostMapping("/adminSearch")
+    public String adminSearch(
+        @RequestParam(required = false) Integer id,
+        @RequestParam(required = false) String product,
+        @RequestParam(required = false) String manufacturer,
+        @RequestParam(required = false) Integer serviceLife,
+        @RequestParam(required = false) String location,
+        Model model) {
+
+        List<FacilityBean> result = facilityService.search(id, product, manufacturer, serviceLife, location);
+
+        List<FacilityForm> forms = new ArrayList<>();
+        for (FacilityBean bean : result) {
+            FacilityForm form = new FacilityForm();
+            BeanUtils.copyProperties(bean, form);
+            forms.add(form);
+        }
+
+        model.addAttribute("facilities", forms);
+        return "admin/admin-facility-list";
+    }
+
+
     //admin/listにGET要求
     @GetMapping("/list")
     String list(Model model) {
@@ -51,6 +82,34 @@ public class AdminController {
             return "admin/register";
         }
         facilityService.save(facilityForm);
+        return "redirect:list";
+    }
+
+    //選択した項目を編集画面に表示（1つ選択可能）
+    @PostMapping(path = "editSelected")
+    public String editSelected(@RequestParam List<Integer> selectedIds, Model model) {
+        Integer id = selectedIds.get(0);
+        FacilityForm facilityForm = facilityService.findOne(id);
+        model.addAttribute("facilityForm", facilityForm);
+        return "admin/edit";
+    }
+
+    // 編集処理
+    @PostMapping(path = "edit")
+    public String edit(@Validated FacilityForm facilityForm, Model model, BindingResult result) {
+        if (result.hasErrors()) {
+            model.addAttribute("facilityForm", facilityForm); // ← これを忘れずに！
+            return "admin/edit";
+        }
+        facilityService.update(facilityForm);
+        return "redirect:list";
+    }
+
+    // 削除処理
+    @PostMapping(path = "delete")
+    public String delete(@RequestParam("selectedIds") List<Integer> selectedIds) {
+        Integer id = selectedIds.get(0);
+        facilityService.delete(id);
         return "redirect:list";
     }
 
--- src/main/java/jp/te4a/app2/facility2/controller/ReadonlyController.java
+++ src/main/java/jp/te4a/app2/facility2/controller/ReadonlyController.java
@@ -4,9 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import jp.te4a.app2.facility2.bean.FacilityBean;
 import jp.te4a.app2.facility2.form.FacilityForm;
 import jp.te4a.app2.facility2.service.FacilityService;
 
@@ -19,6 +27,29 @@ public class ReadonlyController {
     @ModelAttribute
     FacilityForm setUpForm() {
         return new FacilityForm();
+    }
+
+        //管理者ログイン時の検索
+    @PostMapping("/readonlySearch")
+    public String adminSearch(
+        @RequestParam(required = false) Integer id,
+        @RequestParam(required = false) String product,
+        @RequestParam(required = false) String manufacturer,
+        @RequestParam(required = false) Integer serviceLife,
+        @RequestParam(required = false) String location,
+        Model model) {
+
+        List<FacilityBean> result = facilityService.search(id, product, manufacturer, serviceLife, location);
+
+        List<FacilityForm> forms = new ArrayList<>();
+        for (FacilityBean bean : result) {
+            FacilityForm form = new FacilityForm();
+            BeanUtils.copyProperties(bean, form);
+            forms.add(form);
+        }
+
+        model.addAttribute("facilities", forms);
+        return "readonly/readonly-facility-list";
     }
 
     //facilitiesにGET要求
